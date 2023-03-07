bb788ed7da527aebc2ea576640d96b2712b5c3d9
Tanvir Mahin <tanvirmahin24@gmail.com>
2023-03-07 09:38:01 +0600

    create playlist form add

--- src/components/shared/Layout/PlaylistSidebar/PlaylistSidebar.tsx
+++ src/components/shared/Layout/PlaylistSidebar/PlaylistSidebar.tsx
@@ -1,17 +1,38 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { AiOutlineClose } from "react-icons/ai";
 import { connect } from "react-redux";
+import { playlistSidebarToggle } from "../../../../redux/actions/playlist.action";
 import styles from "./PlaylistSidebar.module.css";
 
-type Props = { playlist: any };
+type Props = { playlist: any; playlistSidebarToggle: any };
 
-const PlaylistSidebar = ({ playlist }: Props) => {
+const PlaylistSidebar = ({ playlist, playlistSidebarToggle }: Props) => {
+  const [showCreate, setShowCreate] = useState<boolean>(false);
+  const [name, setName] = useState<string>("");
+  const showCreateHandeler = () => {
+    // LOGIC FOR STRIPE
+    setShowCreate(!showCreate);
+  };
+
+  const submitHandeler = (e: any) => {
+    e.preventDefault();
+  };
   return (
     <div
       className={`${styles.wrapper} ${
         playlist.sidebar_active ? styles.active : ""
       }`}
     >
-      <Container className="py-5">
+      <Container className="pb-5">
+        <div className="d-flex justify-content-end">
+          <span
+            className={styles.close}
+            onClick={() => playlistSidebarToggle()}
+          >
+            <AiOutlineClose />
+          </span>
+        </div>
         {playlist.selected_track ? (
           <>
             <span>{playlist.selected_track.title}</span>
@@ -25,6 +46,27 @@ const PlaylistSidebar = ({ playlist }: Props) => {
         {playlist.playlist.map((pl: any) => (
           <span>{pl.name}</span>
         ))}
+
+        <hr />
+
+        <button onClick={showCreateHandeler} className={styles.btn}>
+          Create New Playlist
+        </button>
+        <Form onSubmit={submitHandeler}>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Playlist Name</Form.Label>
+            <Form.Control
+              value={name}
+              onChange={(e: any) => setName(e.target.value)}
+              type="name"
+              placeholder="Your new playlist"
+            />
+          </Form.Group>
+
+          <button type="submit" className={styles.btn}>
+            Create
+          </button>
+        </Form>
       </Container>
     </div>
   );
@@ -32,4 +74,6 @@ const PlaylistSidebar = ({ playlist }: Props) => {
 const mapStateToProps = (state: any) => ({
   playlist: state.playlist,
 });
-export default connect(mapStateToProps, {})(PlaylistSidebar);
+export default connect(mapStateToProps, { playlistSidebarToggle })(
+  PlaylistSidebar
+);
